f4c1e08a8e0e802f0904db7f533839b0d6c7c99c
이예현 <shszyh@iclooud.com>
2018-09-20 16:47:23 +0900

    03

--- 02.c
+++ 02.c
@@ -5,13 +5,12 @@
 
 int main(int argc, char *argv[]) {
 	char c;
-	int i;
 	
-	printf("input a number:");
+	printf("enter a character:");
 	scanf("%c",&c);
 	
-	i=c-'0';
-	printf("the input number is %i\n",i);
+	printf("the next character of %c is %c\n",c,c+1);
+	
 	
 	return 0;
 }
